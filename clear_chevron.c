f992ac7c5252c599dbb777ef17934363eed98457
sorakann iMac <ski@student.42lausanne.com>
2022-05-10 10:09:58 +0200

    norminette: clear_chevron.c

--- clear_chevron.c
+++ clear_chevron.c
@@ -6,25 +6,27 @@
 /*   By: ski <ski@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/09 20:44:03 by sorakann          #+#    #+#             */
-/*   Updated: 2022/05/10 10:00:54 by ski              ###   ########.fr       */
+/*   Updated: 2022/05/10 10:09:32 by ski              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.h"
 
 /* ************************************************************************** */
-static void delete_chevron_and_file(char **array, int *i);
+static void	delete_chevron_and_file(char **array, int *i);
 
 /* ************************************************************************** */
-void clear_chevron(char **array)
+void	clear_chevron(char **array)
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (array[i])
 	{
-		if (does_word_match(array[i], "<") || does_word_match(array[i], "<<") 
-			|| does_word_match(array[i], ">") || does_word_match(array[i], ">>"))
+		if (does_word_match(array[i], "<")
+			|| does_word_match(array[i], "<<")
+			|| does_word_match(array[i], ">")
+			|| does_word_match(array[i], ">>"))
 		{
 			delete_chevron_and_file(array, &i);
 		}
@@ -33,18 +35,16 @@ void clear_chevron(char **array)
 }
 
 /* ************************************************************************** */
-static void delete_chevron_and_file(char **array, int *i)
+static void	delete_chevron_and_file(char **array, int *i)
 {
-	int j;
-	char *ptr_chevron_str;
-	char *ptr_filename;
+	int		j;
+	char	*ptr_chevron_str;
+	char	*ptr_filename;
 
 	ptr_chevron_str = NULL;
 	ptr_filename = NULL;
-	
-	ptr_chevron_str = array[*i];	
+	ptr_chevron_str = array[*i];
 	ptr_filename = array[*i + 1];
-	
 	j = 0;
 	while (array[*i + j + 2])
 	{
@@ -53,12 +53,9 @@ static void delete_chevron_and_file(char **array, int *i)
 	}
 	array[*i + j] = array[*i + j + 2];
 	array[*i + j + 1] = NULL;
-	
-	
-	ft_free_null((void **)&ptr_chevron_str);		
+	ft_free_null((void **)&ptr_chevron_str);
 	ft_free_null((void **)&ptr_filename);
 	(*i)--;
-
 }
 
 /* ************************************************************************** */
